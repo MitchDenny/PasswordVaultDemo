aed06d663ee8b8ac7d9cd7fdc9abbd8aecff621b
Mitch Denny <mitch@mitchdenny.com>
2013-08-17 18:24:12 +1000

    Added in a basic view model and change the type of page.

---
+++ PasswordVaultDemo/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using PasswordVaultDemo.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.Security.Credentials;
+using Windows.UI.Popups;
+
+namespace PasswordVaultDemo
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            AddCredentialCommand = new RelayCommand(AddCredential);
+        }
+
+        public ICommand AddCredentialCommand { get; private set; }
+
+        public async void AddCredential()
+        {
+            var dialog = new MessageDialog("This dialog will allow you to add a credential.", "Add Credential");
+            await dialog.ShowAsync();
+        }
+
+        public ICommand RemoveCredentialCommand { get; private set; }
+
+        public async void RemoveCredential()
+        {
+            var dialog = new MessageDialog("This dialog will prompt you to confirm removing credentials.", "Remove Credetial");
+            await dialog.ShowAsync();
+        }
+
+        public IEnumerable<PasswordCredential> Credentials { get; set; }
+    }
+}
